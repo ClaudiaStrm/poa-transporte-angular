d3b41b63f73d0262506b127c1f0c411f509f8f23
ClaudiaStrm <s_moura@hotmail.com>
2019-02-28 13:55:06 -0300

    muda paginação para português

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule }    from '@angular/common/http'
 
-import {NgxPaginationModule} from 'ngx-pagination'
+import { NgxPaginationModule } from 'ngx-pagination'
 
 import { AppComponent } from './app.component'
 import { LinhasComponent } from './linhas/linhas.component'
